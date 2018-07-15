277f8a6e4697ae71dcd5af6f57e8f8d32b662721
Bob Reilley <rpreilley@gmail.com>
2018-07-15 15:23:30 -0500

    layout changes

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div>
-    <v-jumbotron>
-      <v-container fill-height>
-        <v-layout row>
-          <v-flex>
-            <h3 class="display-3">Baton Rouge Local Data</h3>
-            <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
-            <v-divider class="my-3"></v-divider>
-            <div class="title mb-3">Enjoy the site.</div>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-jumbotron>
-  </div>
+  <v-container fluid>
+    <v-layout>
+      <v-flex>
+        <v-jumbotron>
+          <h3 class="display-3">Baton Rouge Local Data</h3>
+          <span class="subheading">Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+          <v-divider class="my-3"></v-divider>
+          <div class="title mb-3">Enjoy the site.</div>
+        </v-jumbotron>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
--- src/pages/crime/Crime.vue
+++ src/pages/crime/Crime.vue
@@ -1,107 +1,111 @@
 <template>
-  <div>
-    <v-jumbotron>
-      <v-container fill-height>
-        <v-layout>
-          <v-flex>
+    <v-container fluid>
+      <v-layout>
+        <v-flex>
+          <v-jumbotron>
             <h3 class="display-3">Baton Rouge Crime Map</h3>
 
             <span class="subheading">This map will show attempted and committed crime locations by crime type on a map of Baton Rouge, Louisiana.</span>
-
             <v-divider class="my-3"></v-divider>
-
-            <div class="title mb-3">Please select a crime type.</div>
-            <v-select
-              :items="selections"
-              label="Crime Type"
-              item-text="title"
-              item-value="value"
-              v-model="selection"
-            ></v-select>
-            <v-btn large color="primary" class="mx-0" @click.stop="$router.go(-1)">Back</v-btn>
-            <v-btn
-              class="mx-0"
-              color="primary"
-              large
-              @click="submit"
-            >
-              Submit
-            </v-btn>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-jumbotron>
-
-    <v-flex class="text-xs-center">
-      <v-progress-circular
-        :size="70"
-        :width="4"
-        color="primary"
-        indeterminate
-        v-show="loading"
-      ></v-progress-circular>
-    </v-flex>
-
-    <v-container>
-      <v-layout>
-        <v-flex xs12>
-          <v-data-table
-            v-show="showTable"
-            v-model="selected"
-            :headers="headers"
-            :items="items"
-            :pagination.sync="pagination"
-            select-all
-            item-key="file_number"
-            class="elevation-1"
-          >
-            <template slot="headers" slot-scope="props">
-              <tr>
-                <th
-                  v-for="header in props.headers"
-                  :key="header.text"
-                  :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
-                  @click="changeSort(header.value)"
-                   class="text-xs-center"
+            <v-layout>
+              <v-flex xs12>
+                <div class="title mb-3">Please select a crime type.</div>
+                <v-select
+                  :items="selections"
+                  label="Crime Type"
+                  item-text="title"
+                  item-value="value"
+                  v-model="selection"
+                ></v-select>
+                <v-btn large color="primary" class="mx-0" @click.stop="$router.go(-1)">Back</v-btn>
+                <v-btn
+                  class="mx-0"
+                  color="primary"
+                  large
+                  @click="submit"
                 >
-                  <v-icon small>arrow_upward</v-icon>
-                  {{ header.text }}
-                </th>
-              </tr>
-            </template>
-            <template slot="items" slot-scope="props">
-              <tr>
-                <td class="text-xs-center">{{ props.item.file_number }}</td>
-                <td class="text-xs-center">{{ props.item.offense_date }}</td>
-                <td class="text-xs-center">{{ props.item.offense_time }}</td>
-                <td class="text-xs-center">{{ props.item.crime }}</td>
-                <td class="text-xs-center">{{ props.item.a_c }}</td>
-                <td class="text-xs-center">{{ props.item.offense_desc }}</td>
-                <td class="text-xs-center">{{ props.item.address }}</td>
-                <td class="text-xs-center">{{ props.item.st_number }}</td>
-                <td class="text-xs-center">{{ props.item.st_dir }}</td>
-                <td class="text-xs-center">{{ props.item.st_name }}</td>
-                <td class="text-xs-center">{{ props.item.st_type }}</td>
-                <td class="text-xs-center">{{ props.item.city }}</td>
-                <td class="text-xs-center">{{ props.item.state }}</td>
-                <td class="text-xs-center">{{ props.item.district }}</td>
-                <td class="text-xs-center">{{ props.item.zone }}</td>
-                <td class="text-xs-center">{{ props.item.subzone }}</td>
-                <td class="text-xs-center">{{ props.item.complete_district }}</td>
-                <td class="text-xs-center">{{ props.item.geolocation_state }}</td>
-                <td class="text-xs-center">{{ props.item.council }}</td>
-                <td class="text-xs-center">{{ props.item.geolocation_city }}</td>
-                <td class="text-xs-center">{{ props.item.crime_prevention_dist }}</td>
-                <td class="text-xs-center">{{ props.item.geolocation_address }}</td>
-                <td class="text-xs-center">{{ props.item.new_zip }}</td>
-                <td class="text-xs-center">{{ props.item.geolocation }}</td>
-              </tr>
-            </template>
-          </v-data-table>
+                  Submit
+                </v-btn>
+              </v-flex>
+              <v-flex xs12 sm6 v-show="0==1">
+                <div class="title mb-3">Map Coverage.</div>
+
+              </v-flex>
+            </v-layout>
+          </v-jumbotron>
         </v-flex>
       </v-layout>
-    </v-container>
-  </div>
+
+    <v-layout row>
+      <v-flex class="text-xs-center">
+        <v-progress-circular
+          :size="70"
+          :width="4"
+          color="primary"
+          indeterminate
+          v-show="loading"
+        ></v-progress-circular>
+      </v-flex>
+    </v-layout>
+
+    <v-layout>
+      <v-flex xs12>
+        <v-data-table
+          v-show="showTable"
+          v-model="selected"
+          :headers="headers"
+          :items="items"
+          :pagination.sync="pagination"
+          select-all
+          item-key="file_number"
+          class="elevation-1"
+        >
+          <template slot="headers" slot-scope="props">
+            <tr>
+              <th
+                v-for="header in props.headers"
+                :key="header.text"
+                :class="['column sortable', pagination.descending ? 'desc' : 'asc', header.value === pagination.sortBy ? 'active' : '']"
+                @click="changeSort(header.value)"
+                 class="text-xs-center"
+              >
+                <v-icon small>arrow_upward</v-icon>
+                {{ header.text }}
+              </th>
+            </tr>
+          </template>
+          <template slot="items" slot-scope="props">
+            <tr>
+              <td class="text-xs-center">{{ props.item.file_number }}</td>
+              <td class="text-xs-center">{{ props.item.offense_date }}</td>
+              <td class="text-xs-center">{{ props.item.offense_time }}</td>
+              <td class="text-xs-center">{{ props.item.crime }}</td>
+              <td class="text-xs-center">{{ props.item.a_c }}</td>
+              <td class="text-xs-center">{{ props.item.offense_desc }}</td>
+              <td class="text-xs-center">{{ props.item.address }}</td>
+              <td class="text-xs-center">{{ props.item.st_number }}</td>
+              <td class="text-xs-center">{{ props.item.st_dir }}</td>
+              <td class="text-xs-center">{{ props.item.st_name }}</td>
+              <td class="text-xs-center">{{ props.item.st_type }}</td>
+              <td class="text-xs-center">{{ props.item.city }}</td>
+              <td class="text-xs-center">{{ props.item.state }}</td>
+              <td class="text-xs-center">{{ props.item.district }}</td>
+              <td class="text-xs-center">{{ props.item.zone }}</td>
+              <td class="text-xs-center">{{ props.item.subzone }}</td>
+              <td class="text-xs-center">{{ props.item.complete_district }}</td>
+              <td class="text-xs-center">{{ props.item.geolocation_state }}</td>
+              <td class="text-xs-center">{{ props.item.council }}</td>
+              <td class="text-xs-center">{{ props.item.geolocation_city }}</td>
+              <td class="text-xs-center">{{ props.item.crime_prevention_dist }}</td>
+              <td class="text-xs-center">{{ props.item.geolocation_address }}</td>
+              <td class="text-xs-center">{{ props.item.new_zip }}</td>
+              <td class="text-xs-center">{{ props.item.geolocation }}</td>
+            </tr>
+          </template>
+        </v-data-table>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
