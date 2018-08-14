814540b50c6fce2e1252ac2ec787f62b18d4f745
Bob Reilley <rpreilley@gmail.com>
2018-08-13 21:49:26 -0500

    dense toolbar

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -26,7 +26,7 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-      <v-toolbar fixed app :clipped-left="clipped">
+      <v-toolbar dense fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Bob Reilley Portfolio</v-toolbar-title>
       <v-spacer></v-spacer>
