1fffb68aeff4abd9e5513d5a08f649d05953c79a
Bob Reilley <rpreilley@gmail.com>
2018-08-14 18:47:31 -0500

    axios revisions

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,14 +1,7 @@
 import axios from 'axios'
 
-import config from '../config.js'
-let baseURL = config.baseURL
-let restApiTimeout = config.restApiTimeout
-
-axios.defaults.withCredentials = true
-
 const instance = axios.create({
-  baseURL: baseURL,
-  timeout: restApiTimeout,
+  baseURL: '',
   withCredentials: false
 })
 
