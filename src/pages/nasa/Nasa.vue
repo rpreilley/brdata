54847a2b4d086a7d18420cfcdd6908d1d00a54f0
Bob Reilley <rpreilley@gmail.com>
2018-08-14 18:00:55 -0500

    nasa pod api work. template work

--- src/pages/nasa/Nasa.vue
+++ src/pages/nasa/Nasa.vue
@@ -10,35 +10,110 @@
     </v-layout>
 
     <v-layout row wrap>
-      <v-flex xs12 sm6 md6>
-        <v-date-picker v-model="date" no-title scrollable>
-          <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
-          <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
-        </v-date-picker>
+      <v-flex xs12 sm4 md4 v-show="this.today">
+        <v-menu
+          ref="menu"
+          :close-on-content-click="true"
+          v-model="menu"
+          :nudge-right="10"
+          :return-value.sync="date"
+          lazy
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="date"
+            :label="this.today"
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-date-picker v-model="date" no-title scrollable>
+            <v-spacer></v-spacer>
+            <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.menu.save(date)">OK</v-btn>
+          </v-date-picker>
+        </v-menu>
       </v-flex>
+
+      <v-flex xs12 sm8 md8 v-show="pod">
+        <v-card>
+          <v-card-media
+            :src="this.image"
+            height="400px"
+          ></v-card-media>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ this.title }}</h3>
+              <v-divider class="my-3"></v-divider>
+              <div>{{ this.description }}</div>
+            </div>
+          </v-card-title>
+
+          <v-card-actions>
+            <v-btn color="primary">Share</v-btn>
+            <v-btn color="primary">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+
     </v-layout>
-    <v-divider class="my-3" vertical></v-divider>
   </v-container>
 </template>
 
 <script>
+import ApiService from '@/services/ApiService.js'
+
 export default {
   name: 'Nasa',
   data () {
     return {
       date: null,
-      menu: false
+      menu: false,
+      today: null,
+      pod: false,
+      title: '',
+      image: '',
+      description: ''
+    }
+  },
+  computed: {
+    src: {
+      get: function () {
+        return this.image
+      },
+      set: function (newValue) {
+        debugger
+      }
     }
   },
   methods: {
+    async dateSelection () {
+      let today = new Date()
+      this.today = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + ('0' + today.getDate()).slice(-2)
 
+      let baseUrl = 'https://api.nasa.gov/planetary/apod?api_key=I5HBsa9k5l5rBvl0dG9MBE7Zi1VaTjrpzg03xFpm&date='
+
+      let fullUrl = `${baseUrl}${this.today}`
+
+      try {
+        let response = await ApiService.get(fullUrl)
+        this.title = response.data.title
+        this.image = response.data.url
+        this.description = response.data.explanation
+        this.pod = true
+      } catch (e) {
+        this.errors.push(e)
+      }
+    }
   },
   created () {
 
   },
   mounted () {
-
+    this.dateSelection()
   }
 }
 </script>
