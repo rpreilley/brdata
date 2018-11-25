a78ca7d4502b61d386bd6a41c1c78548b0518f1f
Bob Reilley <rpreilley@gmail.com>
2018-11-25 11:31:09 -0600

    form inputs not showing. fields sometimes empty. looking further into it

--- src/pages/testingArea/testingArea.vue
+++ src/pages/testingArea/testingArea.vue
@@ -3,7 +3,7 @@
     <v-layout>
       <v-flex xs10 offset-xs1>
         <div>Testing Area</div>
-        <br-text-field
+        <!--<br-text-field
           label="Text Field"
           hint="this is a hint"
           placeholder="this is a placeholder"
@@ -14,23 +14,29 @@
           hint="this is a hint"
           placeholder="this is a placeholder"
           >
-        </br-text-area>
+        </br-text-area>-->
+        <br-form
+          v-bind="fields"
+        >
+        </br-form>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import formFields from './formFields.js'
 
 export default {
   name: 'testingArea',
   components: {
     brTextField: () => import('@/controls/brTextField/brTextField.vue'),
-    brTextArea: () => import('@/controls/brTextArea/brTextArea.vue')
+    brTextArea: () => import('@/controls/brTextArea/brTextArea.vue'),
+    brForm: () => import('@/controls/brForm/brForm.vue')
   },
   data () {
     return {
-
+      fields: formFields
     }
   },
   methods: {
